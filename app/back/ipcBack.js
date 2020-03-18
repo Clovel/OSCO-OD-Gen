85c0fce5dec3b2a44e9e84fc6277e07a5a70cbf9
Clovis Durand <cd.clovel19@gmail.com>
2020-03-18 20:46:49 +0100

    Updated IPC test
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- app/back/ipcBack.js
+++ app/back/ipcBack.js
@@ -1,8 +1,21 @@
 const {ipcMain} = require('electron')
 
-/* Test IPC signal handling */
-const ipc = ipcMain;
-
-ipc.on('test-button-signal', () => {
+ipcMain.on('test-button-signal', () => {
     console.log('[DEBUG] IPC signal received by main process');
-})
+});
+
+ipcMain.on('methodCall', async (event, { method, callId, args }) => {
+    const result = {
+        response: null,
+        error: null,
+    };
+
+    try {
+        result.response = await methods[method](...args);
+    }
+    catch (e) {
+        result.error = e;
+    }
+
+    event.reply(`methodReply-${callId}`, result);
+});
--- app/front/ipcFront.js
+++ app/front/ipcFront.js
@@ -1,5 +1,29 @@
-const ipc = require('electron').ipcRenderer;
+const { ipcRenderer } = window.require('electron');
 
-document.getElementById('ipc').addEventListener('click', () => {
-    ipc.send('test-button-signal');
-})
+document.getElementById('test-button').addEventListener('click', () => {
+    ipcRenderer.send('test-button-signal');
+});
+
+let callCounter = 0;
+const createCallId = () => callCounter++;
+
+// export const Methods = new Proxy({}, {
+//     get: (obj, method) => (...args) => new Promise((resolve, reject) => {
+//         const callId = createCallId();
+
+//         ipcRenderer.on(`methodReply-${callId}`, (event, { response, error }) => {
+//             if (error) {
+//                 reject(error);
+//             }
+//             else {
+//                 resolve(response);
+//             }
+//         });
+
+//         ipcRenderer.send('methodCall', {
+//             method,
+//             callId,
+//             args,
+//         });
+//     }),
+// });
