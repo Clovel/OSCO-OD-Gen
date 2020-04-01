901fa0011081a1ff5902c451d966b6b770f386d7
Clovis Durand <cd.clovel19@gmail.com>
2020-04-01 15:48:55 +0200

    [#13] Added OSCOODSubIndex class
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ generator/inc/OSCOODSubIndex.hpp
@@ -0,0 +1,48 @@
+/**
+ * @brief OSCOODSubIndex class
+ * 
+ * @file OSCOODSubIndex.hpp
+ */
+
+#ifndef OSCOODSUBINDEX_HPP
+#define OSCOODSUBINDEX_HPP
+
+/* Includes -------------------------------------------- */
+#include "OSCOODObject.hpp"
+
+/* Library/API export defines */
+#include "APIExports.h"
+
+/* C System */
+#include <cstdint>
+
+/* Defines --------------------------------------------- */
+
+/* Type definitions ------------------------------------ */
+
+/* Forward declarations -------------------------------- */
+
+/* OSCOODSubIndex class -------------------------------- */
+class API_EXPORT OSCOODSubIndex : OSCOODObject {
+    public:
+        /* Constructors */
+        OSCOODSubIndex();
+        OSCOODSubIndex(const uint16_t &pIndex, const uint8_t &pSubIndex);
+
+        /* Destructors */
+        virtual ~OSCOODSubIndex();
+
+        /* Getters */
+        uint16_t index(void) const;
+        uint16_t subIndex(void) const;
+
+        /* Setters */
+        void setIndex(const uint16_t &pIndex);
+        void setSubIndex(const uint16_t &pSubIndex);
+    protected:
+    private:
+        uint16_t mIndex;
+        uint8_t mSubIndex;
+};
+
+#endif /* OSCOODSUBINDEX_HPP */
---
+++ generator/src/OSCOODSubIndex.cpp
@@ -0,0 +1,58 @@
+/**
+ * @brief OSCOODSubIndex class implementation 
+ * 
+ * @file OSCOODSubIndex.hpp
+ */
+
+/* Includes -------------------------------------------- */
+#include "OSCOODSubIndex.hpp"
+
+/* Library/API export defines */
+#include "APIExports.h"
+
+/* C System */
+#include <cstdint>
+
+/* Defines --------------------------------------------- */
+
+/* Type definitions ------------------------------------ */
+
+/* Forward declarations -------------------------------- */
+
+/* OSCOODSubIndex class implementation ----------------- */
+/* Constructors */
+OSCOODSubIndex::OSCOODSubIndex() :
+    OSCOODObject()
+{
+    /* Empty */
+}
+OSCOODSubIndex::OSCOODSubIndex(const uint16_t &pIndex, const uint8_t &pSubIndex) :
+    OSCOODObject(),
+    mIndex(pIndex),
+    mSubIndex(pSubIndex)
+{
+    /* Empty */
+}
+
+/* Destructors */
+OSCOODSubIndex::~OSCOODSubIndex() {
+    /* Empty */
+}
+
+/* Getters */
+uint16_t OSCOODSubIndex::index(void) const {
+    return mIndex;
+}
+
+uint16_t OSCOODSubIndex::subIndex(void) const {
+    return mSubIndex;
+}
+
+/* Setters */
+void OSCOODSubIndex::setIndex(const uint16_t &pIndex) {
+    mIndex = pIndex;
+}
+
+void OSCOODSubIndex::setSubIndex(const uint16_t &pSubIndex) {
+    mSubIndex = pSubIndex;
+}
