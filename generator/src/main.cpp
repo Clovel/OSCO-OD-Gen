c34e2ec8b9d32fd7c79d36e7f8964682333beb9e
Clovis Durand <cd.clovel19@gmail.com>
2020-04-08 20:22:22 +0200

    Removed useless commented code in main.cpp
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -43,41 +43,11 @@ int main(const int argc, const char * const * const argv) {
 
     const std::string lEDSFile = std::string(argv[1U]);
 
-    // /* Create an EDS instance */
-    // EDS *lEDS = nullptr;
-    // try {
-    //     std::cout << "[DEBUG] Opening EDS file " << argv[1U] << std::endl;
-    //     lEDS = new EDS(lEDSFile);
-    //     std::cout << "[INFO ] Successfully parsed EDS file " << argv[1U] << " !" << std::endl;
-    // } catch (const std::exception &e) {
-    //     std::cerr << "[ERROR] Failed to parse EDS file " << argv[1U] << " !" << std::endl;
-    //     return EXIT_FAILURE;
-    // }
-
-    // /* Check the EDS file */
-    // if(0 == lEDS->check()) {
-    //     std::cout << "[INFO ] EDS file check : OK" << std::endl;
-    // } else {
-    //     std::cerr << "[ERROR] EDS file check : KO" << std::endl;
-    //     delete lEDS;
-    //     return EXIT_FAILURE;
-    // }
-
-    /* Generating the same EDS file */
-    // std::string lCopyEDSFile = lEDSFile.substr(0U, lEDSFile.find_last_of('.')) + ".copy.ini";
-    // std::cout << "[DEBUG] lCopyEDSFile = " << lCopyEDSFile << std::endl;
-    // if(0 > lEDS->generateFile(lCopyEDSFile)) {
-    //     std::cerr << "[ERROR] Failed to generate copy if ini file ! " << std::endl;
-    //     delete lEDS;
-    //     return EXIT_FAILURE;
-    // }
-
     /* Generate an OSCO Object Dictionary */
     OSCOOD* lOD = OSCOODFactory::buildOSCOOD(lEDSFile);
     if(nullptr == lOD) {
         std::cerr << "[ERROR] OSCOODFactory::buildOSCOOD failed" << std::endl;
 
-        //delete lEDS;
         delete lOD;
 
         return EXIT_FAILURE;
@@ -85,7 +55,6 @@ int main(const int argc, const char * const * const argv) {
         std::cout << "[INFO ] OSCOODFactory::buildOSCOOD successfully created an Object Dictionary" << std::endl;
     }
 
-    //delete lEDS;
     delete lOD;
     return EXIT_SUCCESS;
 }
