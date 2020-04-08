eccc9320839dcbe78a38e28490e23caa08148be4
Clovis Durand <cd.clovel19@gmail.com>
2020-04-08 20:28:40 +0200

    OSCO-OD-Gen back-end now has a RESTServer instance
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -10,6 +10,8 @@
 
 #include "EDS.hpp"
 
+#include "RESTServer.hpp"
+
 /* C++ system */
 #include <iostream>
 #include <string>
@@ -18,10 +20,12 @@
 #include <cstring>
 
 /* Defines --------------------------------------------- */
+#define ADDRESS "localhost"
 
 /* Notes ----------------------------------------------- */
 
 /* Variable declaration -------------------------------- */
+std::unique_ptr<RESTServer> gRESTServer;
 
 /* Type definitions ------------------------------------ */
 
@@ -29,19 +33,24 @@
 static void printUsage(const char * const pProgName)
 {
     std::cout << "[USAGE] %s" << pProgName << std::endl;
-    std::cout << "        <arg1> : EDS file" << std::endl;
+    std::cout << "        <arg1> : REST API port number" << std::endl;
+    std::cout << "        <arg2> : EDS file" << std::endl;
 }
 
 /* ----------------------------------------------------- */
 /* Main tests ------------------------------------------ */
 /* ----------------------------------------------------- */
 int main(const int argc, const char * const * const argv) {
-    if ((argc < 2) || (std::strcmp(argv[1U], "--help") == 0)) {
+    if ((argc < 3) || (argc > 3) || (std::strcmp(argv[1U], "--help") == 0)) {
         printUsage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    const std::string lEDSFile = std::string(argv[1U]);
+    const std::string lAddr = ADDRESS;
+    const std::string lPort = std::string(argv[1U]);
+    const std::string lPath = "OSCO-OD-Gen";
+
+    const std::string lEDSFile = std::string(argv[2U]);
 
     /* Generate an OSCO Object Dictionary */
     OSCOOD* lOD = OSCOODFactory::buildOSCOOD(lEDSFile);
@@ -53,6 +62,22 @@ int main(const int argc, const char * const * const argv) {
         return EXIT_FAILURE;
     } else {
         std::cout << "[INFO ] OSCOODFactory::buildOSCOOD successfully created an Object Dictionary" << std::endl;
+    }
+
+    /* Set up the REST API Server */
+    gRESTServer = std::unique_ptr<RESTServer>(new RESTServer(lAddr, lPort, lPath));
+
+    /* Open server socket */
+    if(!gRESTServer->open()) {
+        return EXIT_FAILURE;
+    }
+
+    /* Loop for the REST API Server */
+    gRESTServer->listen();
+
+    /* Close server */
+    if(!gRESTServer->close()) {
+        return EXIT_FAILURE;
     }
 
     delete lOD;
