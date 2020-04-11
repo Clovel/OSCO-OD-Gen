12865e4cc803b08f7276ddf974a560b00fefd1a6
Clovis Durand <cd.clovel19@gmail.com>
2020-04-11 21:35:46 +0200

    Fixed App.tsx indentation
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -7,25 +7,20 @@ import BackEndLauncherButton from './../components/BackEndLauncher/BackEndLaunch
 // var lButton = CustomButton;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {BackEndLauncherButton()}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                {BackEndLauncherButton()}
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
