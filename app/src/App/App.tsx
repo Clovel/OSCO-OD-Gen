ee5e3c884db36cd8377e7910d784eccb505218ae
Clovis Durand <cd.clovel19@gmail.com>
2020-04-13 14:09:20 +0200

    Sidebar + Centered other content
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './../logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
+import MainSideBar from './../components/MainSideBar/MainSideBar';
 import BackEndLauncherButton from './../components/BackEndLauncher/BackEndLauncherButton';
+
+const React = require('react');
+const logo = require ('./../logo.svg');
+
 
 // var lButton = CustomButton;
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <BackEndLauncherButton />
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <BackEndLauncherButton />
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                </header>
+            </div>
+            <div>
+                <MainSideBar />
+            </div>
         </div>
     );
 }
---
+++ app/src/components/MainSideBar/MainSideBar.jsx
@@ -0,0 +1,121 @@
+import './MainSideBar.css'
+import React from 'react';
+import { Sidebar, Grid, Icon, GridColumn, Menu, List, ListItem } from 'semantic-ui-react';
+
+const { ipcRenderer } = window.require('electron');
+// const {Header, Icon, Image, Menu, Segment, Sidebar} = require('semantic-ui-react');
+
+const lVisible = true;
+
+class MainSideBar extends React.Component {
+    render() {
+        return (
+            <Sidebar
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                inverted
+                onHide={() => {lVisible = false;}}
+                vertical
+                visible={lVisible}
+                width='thin'>
+                <Menu.Item as='a'>
+                    <Icon name='home'/>
+                    Home
+                </Menu.Item>
+                <Menu.Item as='a'>
+                    <Icon name='cog'/>
+                    Generate C code
+                </Menu.Item>
+                <Menu.Item as='a'>
+                    <Icon name='save'/>
+                    Generate OD description file
+                </Menu.Item>
+                <Menu.Item as='a'>
+                    <Icon name='camera'/>
+                    Channels
+                </Menu.Item>
+            </Sidebar>
+        );
+    }
+}
+
+// function MainSideBar(props) {
+//     return(
+//         <Grid>
+//             <GridColumn>
+//                 <Menu.Item as='a'>
+//                     <Icon name='home'/>
+//                     Home
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='cog'/>
+//                     Generate C code
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='save'/>
+//                     Generate OD description file
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='camera'/>
+//                     Channels
+//                 </Menu.Item>
+//             </GridColumn>
+//             <GridColumn>
+//                 <Menu.Item as='a'>
+//                     <Icon name='home'/>
+//                     Home
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='cog'/>
+//                     Generate C code
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='save'/>
+//                     Generate OD description file
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='camera'/>
+//                     Channels
+//                 </Menu.Item>
+//             </GridColumn>
+//         </Grid>
+//     )
+// }
+
+// function MainSideBar(props) {
+//     var lVisible = true;
+
+//     return (
+//         // <Sidebar.Pushable as={Segment}>
+//             <Sidebar
+//                 as={Menu}
+//                 animation='overlay'
+//                 icon='labeled'
+//                 inverted
+//                 onHide={() => {lVisible = false;}}
+//                 vertical
+//                 visible={lVisible}
+//                 width='thin'>
+//                 <Menu.Item as='a'>
+//                     <Icon name='home'/>
+//                     Home
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='cog'/>
+//                     Generate C code
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='save'/>
+//                     Generate OD description file
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>
+//                     <Icon name='camera'/>
+//                     Channels
+//                 </Menu.Item>
+//             </Sidebar>
+//         // </Sidebar.Pushable>
+//     )
+// }
+
+export default MainSideBar;
