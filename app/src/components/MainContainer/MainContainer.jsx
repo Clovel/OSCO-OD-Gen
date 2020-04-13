a8cf4f2c83c9eda899cf9cbaf5eace454bd00650
Clovis Durand <cd.clovel19@gmail.com>
2020-04-13 15:10:51 +0200

    Fixed MainSideBar width & MainContainer margin
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- app/src/components/MainContainer/MainContainer.jsx
+++ app/src/components/MainContainer/MainContainer.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './MainContainer.css'
 
 import ReactHome from './../ReactHome/ReactHome';
+import {Container} from 'semantic-ui-react';
+import ODTab from './../ODTab/ODTab';
 
 class MainContainer extends React.Component {
     render() {
         return (
             <div class="MainContainer" className="MainContainer">
-                <ReactHome />
+                <Container>
+                    <ReactHome />
+                    <ODTab />
+                </Container>
             </div>
         );
     }
