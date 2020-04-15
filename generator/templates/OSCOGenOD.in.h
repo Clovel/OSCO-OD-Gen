10ae754430f94e06ff8260b295e0a5bc2812ef30
Clovis Durand <cd.clovel19@gmail.com>
2020-04-15 12:11:50 +0200

    [#41] Copied & renamed OD files from OSCO project to create templates
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ generator/templates/OSCOGenOD.in.h
@@ -0,0 +1,15 @@
+/*
+ * @brief @@OD_NAME@@ OD generated header
+ * 
+ * @file GeneratedOD.h
+ */
+
+#ifndef OSCO_GEN_OD_H
+#define OSCO_GEN_OD_H
+
+/* Includes -------------------------------------------- */
+
+/* Defines --------------------------------------------- */
+#define OD_OBJECT_COUNT @@OD_OBJECT_COUNT@@
+
+#endif /* OSCO_GEN_OD_H */
